f681d9409df1e3388af5404e45cb233f80d45add
FroggyTuber <javitxus03@gmail.com>
2025-02-10 21:55:22 +0100

    Compiled for android

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
-  production: true
+  production: true,
+  apiKey: 'b9184b5ec517ab64c1f90721eb68b981',
+  baseUrl: 'https://api.themoviedb.org/3',
+  imageUrl: 'https://image.tmdb.org/t/p',
 };
